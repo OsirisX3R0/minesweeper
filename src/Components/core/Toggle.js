7fbddebff3972550d37d3cdeba3a77c692c1aaa5
Richard Harris <richardharris916@gmail.com>
2022-12-31 08:22:30 -0600

    Moved labels outside of Toggle component

--- src/Components/core/Toggle.js
+++ src/Components/core/Toggle.js
@@ -1,31 +1,20 @@
 import React, { useContext } from "react";
-import {
-  ToggleButton,
-  SwitchOuter,
-  SwitchInput,
-  SwitchSlider,
-  FlagText,
-  OpenText,
-} from "../../Styles";
+import { SwitchOuter, SwitchInput, SwitchSlider } from "../../Styles";
 import { GameContext } from "../../Context/GameContext";
 
 const Toggle = () => {
   const { mode, setMode } = useContext(GameContext);
   return (
-    <>
-      <FlagText selected={mode === "flag"}>flag</FlagText>
-      <SwitchOuter>
-        <SwitchInput
-          type="checkbox"
-          checked={mode === "open"}
-          onChange={() =>
-            setMode((prevMode) => (prevMode === "open" ? "flag" : "open"))
-          }
-        />
-        <SwitchSlider checked={mode === "open"} />
-      </SwitchOuter>
-      <OpenText selected={mode === "open"}>open</OpenText>
-    </>
+    <SwitchOuter>
+      <SwitchInput
+        type="checkbox"
+        checked={mode === "open"}
+        onChange={() =>
+          setMode((prevMode) => (prevMode === "open" ? "flag" : "open"))
+        }
+      />
+      <SwitchSlider checked={mode === "open"} />
+    </SwitchOuter>
   );
 };
 
--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
-import { Foot } from "../../Styles";
+import { Foot, FlagText, OpenText } from "../../Styles";
 import Toggle from "../core/Toggle";
+import { GameContext } from "../../Context/GameContext";
 
 const Footer = () => {
+  const { mode } = useContext(GameContext);
   return (
     <Foot>
+      <FlagText selected={mode === "flag"}>flag</FlagText>
       <Toggle />
+      <OpenText selected={mode === "open"}>open</OpenText>
     </Foot>
   );
 };
